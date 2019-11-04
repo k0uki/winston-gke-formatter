045aa7993d18aab33fa3e632eff1ff760d934e76
k0uki <k0uki221@gmail.com>
2019-11-04 21:29:56 +0900

    Revert "Add build config"
    This reverts commit 3ac1639a50b62d14584e5a896ede186911a13af4.

--- dist/src/format.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const winston_1 = require("winston");
-exports.severity = winston_1.format((info, opt) => {
-    info.severity = info.level;
-    if (!opt.use_level)
-        delete info.level;
-    return info;
-});
